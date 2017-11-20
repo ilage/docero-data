4cc090562a12dabc270f8ba51e469a050956a63a
i.vasyashin <ilage@aport2000.ru>
2017-11-20 12:47:22 +0300

    small refactoring of DDataObjectFactory

--- src/main/java/org/docero/data/utils/DDataAbstractObjectFactory.java
+++ src/main/java/org/docero/data/utils/DDataAbstractObjectFactory.java
@@ -4,6 +4,25 @@ import org.apache.ibatis.reflection.factory.ObjectFactory;
 import java.util.*;
 
 public abstract class DDataAbstractObjectFactory implements ObjectFactory {
+    abstract <T> Class<? extends T> getImplementation(Class<T> type);
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> T create(Class<T> type) {
+        if (List.class.isAssignableFrom(type)) return (T) new ArrayList();
+        if (Map.class.isAssignableFrom(type)) return (T) new HashMap();
+        if (Set.class.isAssignableFrom(type)) return (T) new HashSet();
+        T bean;
+        Class<? extends T> implType = getImplementation(type);
+        try {
+            bean = implType.newInstance();
+        }
+        catch (InstantiationException | IllegalAccessException e) {
+            bean = null;
+        }
+        return bean;
+    }
+
     @Override
     public void setProperties(Properties properties) {
 
